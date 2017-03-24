ba91ac91dc28bcc7848cdcb72ef9b4d7bac0b224
Rob Drew <rdrew@upei.ca>
2017-03-24 10:49:39 -0300

    .

--- template.php
+++ template.php
@@ -5,3 +5,16 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * islandimagined theme.
  */
+
+
+//######################
+// Manipulation of menus
+//######################
+
+
+//main menu (top) using the menu block module
+function islandimagined_menu_tree__menu_block__1(&$variables) {
+    return '<ul class="main-menu__items dropdown menu" data-dropdown-menu>' . $variables['tree'] . '</ul>';
+}
+
+
