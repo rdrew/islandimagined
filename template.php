3b522d3642e07af3d02fd3d410d6e191b90aac4b
rdrew <robdrewupei@gmail.com>
2019-06-26 11:18:23 -0300

    footer links and blocks + sticky header + seach stuff

--- template.php
+++ template.php
@@ -58,3 +58,12 @@ function islandimagined_menu_local_task($variables) {
 
   }
 }
+
+function islandimagined_form_alter(&$form, &$form_state, $form_id) {
+	// target a single form
+	if($form_id == "islandora_solr_simple_search_form"){
+		$form['simple']['islandora_simple_search_query']['#default_value'] = t('Search IslandScholar:'); // Set a default value for the textfield
+		$form['simple']['islandora_simple_search_query']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search IslandScholar:';}";
+		$form['simple']['islandora_simple_search_query']['#attributes']['onfocus'] = "if (this.value == 'Search IslandScholar:') {this.value = '';}";
+	}
+}
